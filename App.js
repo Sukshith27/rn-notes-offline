31391380548db94970af58f91b181a901b5cc1a9
Sukshith <sukshithzone@gmail.com>
2025-10-25 17:20:22 +0530

    gesture and RNSScreenContentWrapper issue fixed

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { enableScreens } from 'react-native-screens';
 import AppNavigator from './src/navigation/AppNavigator';
+
+enableScreens();
 
 export default function App() {
   return <AppNavigator />;
